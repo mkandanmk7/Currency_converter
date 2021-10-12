80d80be372d70bc361208ae7557cf8e963bc23de
muthu <mkandanmk7@gmail.com>
2021-10-12 17:06:53 +0530

    convert Done

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ const displayData = (data) => {
   }
   // console.log(entries);
 };
+
+btn.addEventListener("click", () => {
+  let currency1 = select[0].value;
+  let currency2 = select[1].value;
+  let value = num.value;
+
+  if (currency1 != currency2) {
+    convert(currency1, currency2, value);
+  } else {
+    alert("Choose Different Currency");
+  }
+
+  // async function convert(curr1, curr2, val) {
+  //   const host = "api.frankfurter.app";
+
+  //   const data = await fetch(
+  //     `http://${host}/latest?amount=${val}&from=${curr1}&to=${curr2}`
+  //   );
+  //   console.log(data);
+  //   const res = data.json();
+  //   console.log(res);
+  // }
+
+  function convert(curr1, curr2, val) {
+    const host = "api.frankfurter.app";
+
+    fetch(`http://${host}/latest?amount=${val}&from=${curr1}&to=${curr2}`)
+      .then((val) => val.json())
+      .then((val) => {
+        console.log(Object.values(val.rates)[0]);
+        ans.value = Object.values(val.rates)[0];
+      });
+  }
+});
