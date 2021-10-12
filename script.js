6ba705a07348e5ffda75cf432b85bfafed5236e8
muthu <mkandanmk7@gmail.com>
2021-10-12 16:31:36 +0530

    option done

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ const ans = document.querySelector("#ans");
 const getData = async () => {
   const data = await fetch("https://api.frankfurter.app/currencies");
   const res = await data.json();
-
-  console.log(res);
+  displayData(res);
+  // console.log(res);
 };
 
 getData();
+
+const displayData = (data) => {
+  // console.log(data);
+  const entries = Object.entries(data);
+  for (var i = 0; i < entries.length; i++) {
+    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+  }
+  // console.log(entries);
+};
